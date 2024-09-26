b8163270ec75eba754ae2fe060db1b453312ccdf
Frizx100 <khoroshev04@gmail.com>
2024-09-26 20:08:57 +0300

    Ya v shokax

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-//const socket = new WebSocket('ws://localhost:4001');
-
-// Обробка отриманих даних через WebSocket
-//socket.onmessage = function (event) {
-   // const data = JSON.parse(event.data);
-    //updateGraph(data);
-//};
 const socket = new WebSocket('ws://localhost:4001');
 var points = new Map();
 
+function millisToSeconds(millis) {
+    var seconds = (millis / 1000);
+    return seconds;
+}
+
 function updatetPointLocation(id, data) {
     points.set(id, { lastUpdate: Date.now(), data: data });
-    var result = { x: [], y: [] };
+    var result = { x: [], y: [],r: [], mode: [], color: [], size:[] };
+    
     for (var pointData of points.entries()) {
         if (Date.now() - pointData[1].lastUpdate >= 5000) {
             points.delete(pointData[0]);
@@ -18,8 +17,30 @@ function updatetPointLocation(id, data) {
         }
         result.x.push(pointData[1].data.x);
         result.y.push(pointData[1].data.y);
+        var rad = millisToSeconds(pointData[1].data.receivedAt-pointData[1].data.sentAt)*300000
+        result.r.push(rad);
+        result.mode.push("markers")
+        result.color.push('red')
+        result.size.push(20)
+    
     }
-    Plotly.update('graph', { x: [result.x], y: [result.y] }, {});
+    if (result.x.length>=3){
+        var a = 2*(result.x[1]-result.x[0])
+        var b = 2*(result.y[1]-result.y[0])
+        var c = result.r[0]**2-result.r[1]**2 - result.x[0]**2+result.x[1]**2 - result.y[0]**2+result.y[1]**2
+        var d = 2*(result.x[2]-result.x[1])
+        var e = 2*(result.y[2]-result.y[1])
+        var f = result.r[1]**2-result.r[2]**2 - result.x[1]**2+result.x[2]**2 - result.y[1]**2+result.y[2]**2
+        var x = (c*e-f*b)/(e*a-b*d)
+        var y = (c*d-a*f)/(b*d-a*e)
+        result.x.push(x);
+        result.y.push(y);
+        result.r.push(0);
+        result.mode.push("markers")
+        result.color.push('blue')
+        result.size.push(20)
+    }
+    Plotly.update('graph', { x: [result.x], y: [result.y], mode:[result.mode], marker:{color:result.color, size:result.size} }, {});
     console.log(result)
 }
 
@@ -34,6 +55,7 @@ socket.onmessage = (event) => {
     console.log('Отримані дані:', event.data);
     const data = JSON.parse(event.data);
     updatetPointLocation(data.id, data);
+
 };
 socket.onclose = () => {
     console.log('З\'єднання закрито');
